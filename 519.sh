68a55d2947a86e4f8da6912ce3008af998ed5961
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-05-19 10:43:31 +0800

    Add files via upload

---
+++ 519.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [ $# -n 1]; then
+ echo "" > &2
+   exit  1
+fi
+
+check4_cmd_in_path "$1"
+case $? in
+   0) echo "exit cmd in path" ;;
+   1) echo "can not found out cmd in path, which remains in doubt." ;;
+   2) echo "cmd not exit in path" ;;
+esac
+
+exit 0
